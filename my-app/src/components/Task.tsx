ffa04630b3d3c68a1c4efe796cc103d98d064d8c
Josh Wedekind <josh@halfnibble.com>
2023-04-02 14:26:54 -0700

    Fix TypeScript errors and warnings.

--- my-app/src/components/Task.tsx
+++ my-app/src/components/Task.tsx
@@ -1,44 +1,80 @@
+/*
+    NOTES:
+    Even though React is not used directly in this file, it is still imported.
+    This is a requrirement to make TypeScript happy with JSX (e.g. <div></div>).
+*/
+import React, { useState, useEffect, KeyboardEvent } from "react";
 import Checkbox from "./Checkbox";
-import {useState, useEffect} from "react";
 
-export default function Task({todo_item,done,onToggle,onTrash,onEnter},props) {
-  const [editMode,setEditMode] = useState(false);
-  const [description,setDescription] = useState('');
+interface ITaskProps {
+    todo_item: string;
+    description: string;
+    setDescription: (e: string) => void;
+    done: boolean;
+    onToggle: (done: boolean) => void;
+    onTrash: () => void;
+    onEnter: (e: KeyboardEvent<HTMLInputElement>) => void;
+}
 
+export default function Task({
+    todo_item,
+    done,
+    description,
+    setDescription,
+    onToggle,
+    onTrash,
+    onEnter,
+}: ITaskProps) {
+    const [editMode, setEditMode] = useState(false);
 
-  useEffect(() => {
-    if (editMode) {
-      const input = document.querySelector('.task input');
-      input.focus();
-      input.setSelectionRange(input.value.length, input.value.length);
-    }
-  }, [editMode]);
+    useEffect(() => {
+        if (editMode) {
+            /*
+                NOTES:
+                Here, I am explicitly setting the type of the input variable to be HTMLInputElement | null.
+                Because I know the querySelector will return an HTMLInputElement or null.
+                The `?.` syntax is called optional chaining. It is a new feature in TypeScript 3.7.
+                It means that if the value before the ?. is null or undefined, the statement after will not execute.
 
-  const handleBlur = () => setEditMode(false);
-  
-    
+            */
+            const input: HTMLInputElement | null = document.querySelector(".task input");
+            input?.focus();
+            input?.setSelectionRange(input.value.length, input.value.length);
+        }
+    }, [editMode]);
 
-  return (
-    <div className={'task ' + (done?'done':'')}>
-      <Checkbox checked={done} onClick={() => onToggle(!done)} />
-      {!editMode && (
-        <div className="task-name" onClick={() => setEditMode(prev => !prev)}>
-          <span>{todo_item}</span>
+    const handleBlur = () => setEditMode(false);
+
+    return (
+        <div className={"task " + (done ? "done" : "")}>
+            <Checkbox checked={done} onClick={() => onToggle(!done)} />
+            {!editMode && (
+                <div className="task-name" onClick={() => setEditMode((prev) => !prev)}>
+                    <span>{todo_item}</span>
+                </div>
+            )}
+            {editMode && (
+                <form
+                    onSubmit={(ev) => {
+                        ev.preventDefault();
+                        setEditMode(false);
+                    }}
+                >
+                    <input
+                        type="text"
+                        defaultValue={todo_item}
+                        onBlur={handleBlur}
+                        onClick={console.log}
+                        onChange={(e) => setDescription(e.target.value)}
+                        onKeyUp={(e) => onEnter(e)}
+                    />
+                </form>
+            )}
+            <button className="trash" onClick={onTrash}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                    <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
+                </svg>
+            </button>
         </div>
-      )}
-      {editMode && (
-        <form onSubmit={ev => {ev.preventDefault();setEditMode(false);} }>
-          <input type="text" defaultValue={todo_item}
-                 onBlur={handleBlur} 
-                 onClick={console.log({description})}
-                onChange={e => setDescription(e.target.value)}
-                onKeyUp={e => onEnter(e)}/>
-        </form>
-      )}
-      <button className="trash" onClick={onTrash}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
-      </button>
-    </div>
-  );
-  console.log({description})
+    );
 }
--- my-app/src/components/TaskForm.tsx
+++ my-app/src/components/TaskForm.tsx
@@ -1,50 +1,60 @@
-import {useState} from "react";
+import React, { useState, FormEventHandler } from "react";
 
-export default function TaskForm({onAdd}) {
+interface TaskFormProps {
+    onAdd: (taskName: string) => void;
+}
 
-  const [description, setDescription] = useState("");
+export default function TaskForm({ onAdd }: TaskFormProps) {
+    const [description, setDescription] = useState("");
 
-  const handleFormSubmit = async e => {
-      e.preventDefault();
-     try { 
-      const body = {description};
-      const response = await fetch("http://localhost:5000/todo",{
-          method: "POST",
-          headers: {"Content-Type": "application/json"},
-          body: JSON.stringify(body)
-      });
-      
-      window.location = '/'
-     } catch (err) {
-          console.log(err)
-     }
-  }
+    /*
+        NOTES:
+        Here, I am explicitly setting the type of the handleFormSubmit variable to be 
+        `FormEventHandler<HTMLFormElement>`.
+        I did not memorize the type for a React Form Submit event handler.
+        Instead, I learned the value by hovering over the `onSubmit` prop of the form element.
+        Then I assumed it must be imported from the React library because it is a JSX attribute.
+    */
+    const handleFormSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+        e.preventDefault();
+        try {
+            const body = { description };
+            const response = await fetch("http://localhost:5000/todo", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(body),
+            });
 
-  // const [taskName,setTaskName] = useState('');
-  // function handleSubmit(ev) {
-  //   ev.preventDefault();
-  //   onAdd(taskName);
-  //   setTaskName('');
+            if (response.status === 200) {
+                window.location.href = "/";
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
-  return (
-    <form onSubmit={handleFormSubmit}>
-      <button>+</button>
-      <input type="text"
-             value={description}
-             onChange={ev => setDescription(ev.target.value)}
-             required
+    // const [taskName,setTaskName] = useState('');
+    // function handleSubmit(ev) {
+    //   ev.preventDefault();
+    //   onAdd(taskName);
+    //   setTaskName('');
+
+    return (
+        <form onSubmit={handleFormSubmit}>
+            <button>+</button>
+            <input
+                type="text"
+                value={description}
+                onChange={(ev) => setDescription(ev.target.value)}
+                required
                 autoFocus
                 maxLength={250}
                 minLength={1}
-             placeholder="Add List..."/>
-      <button 
-                className="btn"
-                aria-label="Add Task"
-                type="submit"
-                >
+                placeholder="Add List..."
+            />
+            <button className="btn" aria-label="Add Task" type="submit">
                 Add Task
-                </button>
-    </form>
-  );
+            </button>
+        </form>
+    );
 }
-
